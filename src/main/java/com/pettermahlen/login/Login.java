51ef090be958c9188ea82cee3ffeb8b662564fce
Petter Måhlén <petter@spotify.com>
2015-11-24 08:39:09 +0100

    remove things to be implemented during demo

--- src/main/java/com/pettermahlen/login/Login.java
+++ src/main/java/com/pettermahlen/login/Login.java
@@ -19,24 +19,6 @@ public class Login {
   }
 
   public Response<String> authenticate(RequestContext requestContext) {
-    final Optional<String> userName = requestContext.request().parameter("userName");
-    final Optional<String> password = requestContext.request().parameter("password");
-
-    if (!userName.isPresent() || !password.isPresent()) {
-      return Response.forStatus(
-          Status.BAD_REQUEST
-              .withReasonPhrase("Both 'userName' and 'password' query parameters must be present"));
-    }
-
-    final Optional<User> user = userStore.findByName(userName.get());
-
-    if (!user.isPresent() || !user.get().password().equals(password.get())) {
-      // you could possibly argue that it's better to return 403 FORBIDDEN here, but that normally
-      // means that the resource is forbidden. In this case, the login resource is accessible, but
-      // the authentication failed.
-      return Response.forPayload("FAILURE");
-    }
-
-    return Response.forPayload("SUCCESS");
+    throw new UnsupportedOperationException();
   }
 }
